1d7dcd9110b15db3bbf2aae7046034ece13df161
Jon Barker <jonathan.t.barker@gmail.com>
2018-11-03 17:03:08 -0500

    changed to a preorder solution

--- twosumBST.py
+++ twosumBST.py
@@ -5,15 +5,27 @@ class Solution:
         :type k: int
         :rtype: bool
         """
-        nums = set()
-        def traverse(root):
-            nums.add(root)
-            if root-k in nums:
-                return True
-            if root.left:
-                traverse(root.left)
-            if root.right:
-                traverse(root.right)
-            else:
-                return root
-        return False
+        
+    
+
+        if root == None:
+            return False
+        self.trav = set()
+        self.k = k
+        self.found = False
+        self.preorder(root)
+        return self.found
+    
+    def preorder(self, node):
+        if self.found:
+            return
+        comp = self.k - node.val
+        if comp in self.trav:
+            self.found = True
+            return 
+        else:
+            self.trav.add(node.val)
+        if self.found:
+            return
+        if node.left: self.preorder(node.left)
+        if node.right: self.preorder(node.right)
