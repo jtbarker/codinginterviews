9222e55f674d513e8b328e0c4cce3c01c4b985b9
Jon Barker <jonathan.t.barker@gmail.com>
2018-11-03 16:36:51 -0500

    added a comment

--- uniqueemailaddresses.py
+++ uniqueemailaddresses.py
@@ -2,7 +2,7 @@ class Solution(object):
     def numUniqueEmails(self, emails):
         seen = set()
         for email in emails:
-            local, _, domain = email.partition('@')
+            local, _, domain = email.partition('@') #partition function is a handy way to split up a string on a specific pattern"
             if '+' in local:
                 local = local[:local.index('+')]
             seen.add(local.replace('.','') + '@' + domain)
