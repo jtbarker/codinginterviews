8d3c3184646f3936e03eea54e8ae2bc7a05a983f
Jon Barker <jonathan.t.barker@gmail.com>
2018-11-04 16:46:27 -0600

    Update deckhasgroupsizeX.py

--- deckhasgroupsizeX.py
+++ deckhasgroupsizeX.py
@@ -4,14 +4,12 @@ class Solution:
         :type deck: List[int]
         :rtype: bool
         """
-        sorteddeck = sorted(deck)
-        sortedsetdeck = set(sorteddeck)
-        counts = []
-        for i in sortedsetdeck:
-            counts.append(sorteddeck.count(i))
-        for i in counts:
-            for j in (i,len(counts)):
-                if i != j:
-                    return False
-                else:
-                    return True
+        def gcd(a,b):
+            while b:
+                a,b = b, a%b
+            return a
+        a = [v for k,v in collections.Counter(deck).items()]
+        ans = a[0]
+        for i in range(1,len(a)):
+            ans = gcd(ans,a[i])
+        return ans >= 2
