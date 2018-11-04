d5aeb57a7ae7b5b4a2af5518f82629b819d85048
Jon Barker <jonathan.t.barker@gmail.com>
2018-11-04 16:47:51 -0600

    Update deckhasgroupsizeX.py

--- deckhasgroupsizeX.py
+++ deckhasgroupsizeX.py
@@ -6,10 +6,10 @@ class Solution:
         """
         def gcd(a,b):
             while b:
-                a,b = b, a%b
+                a,b = b, a%b #how to get the gcd using euclidean algo
             return a
-        a = [v for k,v in collections.Counter(deck).items()]
+        a = [v for k,v in collections.Counter(deck).items()] #get list of values for counts
         ans = a[0]
-        for i in range(1,len(a)):
+        for i in range(1,len(a)): #run through every count value and get the gcd for each pair of counts from beginning to end
             ans = gcd(ans,a[i])
-        return ans >= 2
+        return ans >= 2 # if final value is not 1, true else false
